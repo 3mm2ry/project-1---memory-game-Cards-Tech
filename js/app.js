496b433e90846d3b3ab772453856fb577c36b6a7
Ammar Yaser <amm2rpriv@gmail.com>
2026-08-10 19:46:11 +0300

    declare the Variables and Cached the  Element in js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 console.log('Technologies Memory Match')
+
+/*-------------------------------- Constants --------------------------------*/
+
+     const ChoseDifficulty = ['Easy','Medium','Hard']
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+   let BestScore =null;
+ let Duration =null;
+ let Difficulty=null; 
+ let Lives =null;
+
+/*------------------------ Cached Element References ------------------------*/
+
+const ChooseDifficultyButtons=document.querySelectorAll('.Difficulty')
+const Cards =document.querySelectorAll('.sqr')
+const GameInfoDifficulty =document.querySelector('#Difficulty')
+const GameInfoLives =document.querySelector('#Lives')
+const GameInfoScore =document.querySelector('#Score')
+const GameInfoDuration =document.querySelector('#Duration')
+
+console.log(Cards,GameInfoDifficulty,ChooseDifficultyButtons,GameInfoLives,GameInfoDuration,GameInfoScore)
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
